3eaf43379fb1af97db9767cab4ff3f5fe070e9c7
Gabrielle <gabriellegayno@gmail.com>
2025-01-17 18:53:17 -0300

    call functions hide

--- assets/js/script.js
+++ assets/js/script.js
@@ -147,10 +147,27 @@
     // Skills Slider End
 
     /* ============================================================ */
-    /* Testimonial Sections
+    /* Testimonial Slider Hide
     /* ============================================================ */
-        hidePortfolio();
-        hideTestimonial();
+    function hideTestimonial(){
+        let hideTestimonial = document.getElementById("testimonial");
+        if (testimonial){
+        hideTestimonial.style.display = "none"
+        }
+    }
+    hideTestimonial();
+  
+        /* ============================================================ */
+      /* Testimonial Slider Hide
+      /* ============================================================ */
+  
+      function hidePortfolio(){
+        let hidePortfolio = document.getElementById("portfolio");
+        if (portfolio){
+        hidePortfolio.style.display = "none"
+        }
+    }
+    hidePortfolio();
 
 
     /* ============================================================ */
